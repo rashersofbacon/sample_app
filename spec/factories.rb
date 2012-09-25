c51def79dc4cd733d81eff73c00c373c43f0a727
David Bacon <new_davros80@hotmail.com>
2012-09-25 11:06:45 +1000

    Finish user singup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "David Bacom"
+    email    "new_davros80@hotmail.com"
+    password "12cpziag"
+    password_confirmation "12cpziag"
+  end
+end
